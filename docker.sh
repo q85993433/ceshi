e29e8c8b4ad2820b421ea528b138c9eff9fe7889
q85993433 <395869245@qq.com>
2020-06-18 14:26:22 +0800

    s

---
+++ docker.sh
@@ -0,0 +1,32 @@
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install  -y docker-ce 
+sudo systemctl start docker
+sudo systemctl enable docker
+docker version
+
+
+samba 
+docker run -it --name samba -p 139:139 -p 445:445 -v /opt:/mount -d dperson/samba -u "admin;123456" -s "shared;/mount/;yes;no;no;all;none"
+
+suns:
+因为你关机了
+suns:
+docker ps -a
+suns:
+docker start samba
+
+百度docker容器自启   好像叫—restart  away
+
+root@lch xxl-job-admin]# docker run -d -p 8180:8180 --restart=always --name xxl-job-admin-docker xxl-job-admin-docker
+
+1.在使用docker run时，添加下面参数
+--restart=always 
+
+2.在运行docker的时候添加
+docker update --restart=always 07fb7442f813
+
+其中07fb7442f813是容器ID
+
+docker restart jenkins    重启
+
