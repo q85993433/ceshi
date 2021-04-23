dee946459ba4b2a2f1d742f8eb82506c320978fc
q85993433 <395869245@qq.com>
2021-04-23 09:30:42 +0800

    s

---
+++ NESSUS.sh
@@ -0,0 +1,11 @@
+NESSUS
+
+Nessus家庭版最大只支持扫描16个主机。但利用docker无限使用（当然虚拟机快照也可以）
+
+
+v1:镜像较大，8GB左右，适用于网速较好的情况，不需要等待容器初始化。
+
+v1:
+
+docker run --rm -itd -p 8834:8834 registry.cn-hangzhou.aliyuncs.com/steinven/nessus:v0.1
+访问https://ip:8834，账号：admin密码：admin
